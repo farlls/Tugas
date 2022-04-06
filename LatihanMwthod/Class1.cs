30627ba00f2f46411a22d04221cefb1c9651b722
farlls <farrelshahizdn22@gmail.com>
2022-04-06 11:19:22 +0700

    Add project files.

---
+++ LatihanMwthod/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod
+{
+    class Angka
+    {
+        private int num1, num2;
+        public int FindMinimum(int number1, int number2)
+        {
+            return number1 < number2 ? number1 : number2;
+             
+        }
+        public int FindMinimum(int number1, int number2,float number3)
+        {
+            num1 = number1 < number3 ? number1 : (int)number3;
+            num2 = number2 < number3 ? number2 : (int)number3;
+            return number1 < number2 ? num1 : num2;
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            return number1 > number2 ? number1 : number2;
+        }
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            num1 = number1 > number3 ? number1 : (int)number3;
+            num2 = number2 > number3 ? number2 : (int)number3;
+            return number1 > number2 ? num1 : num2;
+        }
+    }
+}
---
+++ LatihanMwthod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Angka angka = new Angka();
+            Console.WriteLine("Minimun #1: {0}", angka.FindMinimum(3, 1));
+            Console.WriteLine("Minimun #2: {0}", angka.FindMinimum(3, 2, 4));
+            Console.WriteLine();
+            Console.WriteLine("Maximum #1: {0}", angka.FindMaximum(3, 1));
+            Console.WriteLine("Maximum #2: {0}", angka.FindMaximum(3, 2, 4));
+
+            Console.ReadKey();
+        }
+    }
+}
